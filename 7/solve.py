15cf5a44e477e0bc101f7e0d3300d1debaf1ebc1
Rasmus T. Bjerg <rs.bjerg@gmail.com>
2022-12-08 11:10:21 +0100

    ups

--- 7/solve.py
+++ 7/solve.py
@@ -4,7 +4,7 @@ active_dirs = []
 with open("input.txt") as f:
     for line in f:
         line = line.strip()
-        e = line.split[" "]
+        e = line.split(" ")
         first = e[0]
         second = e[1]
         if first == "$":
@@ -22,31 +22,24 @@ with open("input.txt") as f:
                 else:
                     dir_sizes[curr_dir] = int(first)
 
-    # for line in f:
-    #     line = line.strip()
-        # elems = line.split(" ")
-        # f = elems[0]
-        # s = elems[1]
-        # if f == "$" and s == "cd":
-        #     new_dir = elems[2]
-        #     if new_dir == "/":
-        #         active_dirs = ["/"]
-        #     elif new_dir == "..":
-        #         active_dirs.pop()
-        #     else:
-        #         active_dirs.append(new_dir)
-        # elif f != "dir" and s != "ls":
-        #     for d in active_dirs:
-        #         if d in dir_sizes:
-        #             dir_sizes[d] += int(f)
-        #         else:
-        #             dir_sizes[d] = int(f)
-
 def part1():
-    print(sum(s for s in dir_sizes.values() if s <= 100000))
+    sum = 0
+    for size in dir_sizes.values():
+        if size <= 100000:
+            sum += size
+    print(sum)
 
 def part2():
-    pass
+    fs_size = dir_sizes["['/']"]
+    total = 70000000
+    need = 30000000
+    unused = total - fs_size
+    best = None
+    for size in dir_sizes.values():
+        if unused + size >= need and (best == None or size < best):
+            best = size
+    print(best)
+
 
 part1()
 part2()
